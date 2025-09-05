91f6846071f0c2faf152cbff75d567964c5dcaf4
d.starohorodskyi <d.starohorodskyi@plarium.com>
2025-09-05 23:37:20 +0200

    Added interactable gun

---
+++ Assets/Scripts/Gun/Gun.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+
+namespace Gun
+{
+    public class Gun : MonoBehaviour
+    {
+        [SerializeField] private Transform objectToRotate;
+        [SerializeField] private Camera viewCamera;
+        [SerializeField, Min(0.01f)] private float cursorDepthFromCamera = 10f;
+
+        private void Awake()
+        {
+            if (objectToRotate == null)
+            {
+                objectToRotate = transform;
+            }
+
+            if (viewCamera == null)
+            {
+                viewCamera = Camera.main;
+            }
+        }
+
+        private void Update()
+        {
+            if (objectToRotate == null || viewCamera == null)
+            {
+                return;
+            }
+
+            Vector2 screenPos;
+#if ENABLE_INPUT_SYSTEM
+            if (Mouse.current != null)
+            {
+                screenPos = Mouse.current.position.ReadValue();
+            }
+            else
+            {
+                return;
+            }
+#else
+        screenPos = Input.mousePosition;
+#endif
+
+            Vector3 worldPoint = viewCamera.ScreenToWorldPoint(new Vector3(screenPos.x, screenPos.y, cursorDepthFromCamera));
+            objectToRotate.LookAt(worldPoint);
+        }
+    }
+}
