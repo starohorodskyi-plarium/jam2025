11141b0fe7e8c54102aa99821ebda8db8403236d
d.starohorodskyi <d.starohorodskyi@plarium.com>
2025-09-06 17:51:18 +0200

    Added vfx plugin and connected vfx for citizens

--- Assets/Scripts/Core/NPCController.cs
+++ Assets/Scripts/Core/NPCController.cs
@@ -3,9 +3,23 @@ using UnityEngine;
 
 public class NPCController : MonoBehaviour
 {
+    public enum Faction
+    {
+        Ally,
+        Enemy
+    }
+
+    [SerializeField] private Faction faction = Faction.Enemy;
+    [SerializeField] private GameObject allyHitEffect;
+    [SerializeField] private GameObject enemyHitEffect;
     public void Hit()
     {
         Debug.Log($"{gameObject.name} was hit!");
+        GameObject effectPrefab = faction == Faction.Ally ? allyHitEffect : enemyHitEffect;
+        if (effectPrefab != null)
+        {
+            Instantiate(effectPrefab, transform.position, Quaternion.identity);
+        }
         StartCoroutine(HitRoutine());
 
         IEnumerator HitRoutine()
