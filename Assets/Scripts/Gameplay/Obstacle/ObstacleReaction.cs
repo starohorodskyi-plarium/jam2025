57c3f1fd68b1ce2728eae89f6e030f99dc8eaa14
d.starohorodskyi <d.starohorodskyi@plarium.com>
2025-09-06 22:35:30 +0200

    Added obstacles

---
+++ Assets/Scripts/Gameplay/Obstacle/ObstacleReaction.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+
+public class ObstacleReaction : MonoBehaviour
+{
+    [Header("Reaction Prefab")]
+    [SerializeField] private GameObject reactionPrefab;
+
+    public void Hit(float impactDelay, Vector3 collisionPoint)
+    {
+        Debug.Log($"{gameObject.name} was hit!");
+       
+        StartCoroutine(HitRoutine());
+
+        IEnumerator HitRoutine()
+        {
+            yield return new WaitForSeconds(impactDelay);
+            CreateReaction(collisionPoint);
+        }
+    }
+    
+    public void CreateReaction(Vector3 collisionPoint)
+    {
+        if (!reactionPrefab)
+            Debug.LogWarning($"{nameof(ObstacleReaction)}: reactionPrefab is not assigned.", this);
+        else 
+            Instantiate(reactionPrefab, collisionPoint, Quaternion.identity);
+    }
+}
