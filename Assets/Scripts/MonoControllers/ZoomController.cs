6152c68aa348b07dd7479a268e29f0a844686680
o.boiarskyi <o.boiarskyi@plarium.com>
2025-09-07 06:37:49 +0300

    Zoom added

---
+++ Assets/Scripts/MonoControllers/ZoomController.cs
@@ -0,0 +1,40 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace MonoControllers
+{
+    public class ZoomController : MonoBehaviour
+    {
+        public float zoomFOV = 40f;
+        public float normalFOV = 60f;
+        public float duration = 0.5f;
+
+        public event Action<bool> OnZoomChanged; 
+        
+        private bool isZoomed;
+
+        private Camera _camera;
+        
+        private void Awake() => 
+            _camera = Camera.main;
+
+        private void Update()
+        {
+            if (GameManager.Instance.InputEnabled && Input.GetKeyDown(KeyCode.Mouse1))
+                ToggleFOV();
+        }
+        
+        private void ToggleFOV()
+        {
+            if (isZoomed)
+                _camera.DOFieldOfView(normalFOV, duration).SetEase(Ease.OutQuad);
+            else
+                _camera.DOFieldOfView(zoomFOV, duration).SetEase(Ease.OutQuad);
+
+            isZoomed = !isZoomed;
+            
+            OnZoomChanged?.Invoke(isZoomed);
+        }
+    }
+}
---
+++ Assets/Scripts/MonoControllers/ZoomSensitivityUpdater.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+namespace MonoControllers
+{
+    public class ZoomSensitivityUpdater : MonoBehaviour
+    {
+        [SerializeField] private CameraController _cameraController;
+        [SerializeField] private ZoomController _zoomController;
+
+        [SerializeField] private float _zoomSensitivityX = 12;
+        [SerializeField] private float _zoomSensitivityY = 12;
+        [SerializeField] private float _maxAngleX = 1.5f;
+        [SerializeField] private float _maxAngleY = 1.5f;
+
+        private float _defaultSensitivityX;
+        private float _defaultSensitivityY;
+        private float _defaultAngleX;
+        private float _defaultAngleY;
+        
+        private void Awake()
+        {
+            _defaultSensitivityX = _cameraController.sensitivityX;
+            _defaultSensitivityY = _cameraController.sensitivityY;
+        }
+
+        private void OnEnable() => 
+            _zoomController.OnZoomChanged += UpdateSensitivity;
+
+        private void OnDisable() => 
+            _zoomController.OnZoomChanged -= UpdateSensitivity;
+
+        private void UpdateSensitivity(bool zoomed)
+        {
+            _cameraController.sensitivityX = zoomed ? _zoomSensitivityX : _defaultSensitivityX;
+            _cameraController.sensitivityY = zoomed ? _zoomSensitivityY : _defaultSensitivityY;
+            _cameraController.maxXAngle  = zoomed ? _maxAngleX : _defaultAngleX;
+            _cameraController.maxYAngle  = zoomed ? _maxAngleY : _defaultAngleY;
+        }
+    }
+}
